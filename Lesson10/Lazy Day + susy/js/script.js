9ed6c6ecadbbaebeb3c6b0711a5862eb4c0db3b2
qqumn <kilihevich_nikita@ukr.net>
2015-12-13 22:13:56 +0200

    Upload homework. Lesson 10

---
+++ Lesson10/Lazy Day + susy/js/script.js
@@ -0,0 +1,34 @@
+//noinspection JSUnresolvedFunction
+/**
+ * Created by apple on 05.12.15.
+ */
+$(document).ready(function(){
+    $(".slider").simpleSlider();
+    var slider = $(".slider").data("simpleslider");
+    var options = {
+        slides: '.slide', // The name of a slide in the slidesContainer
+        swipe: true,    // Add possibility to Swipe
+        magneticSwipe: true, // Add 'magnetic' swiping. When the user swipes over the screen the slides will attach to the mouse's position
+        transition: "slide", // Accepts "slide" and "fade" for a slide or fade transition
+        slideTracker: true, // Add a UL with list items to track the current slide
+        slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
+        slideOnInterval: true, // Slide on interval
+        interval: 3000, // Interval to slide on if slideOnInterval is enabled
+        animateDuration: 1500, // Duration of an animation
+        animationEasing: 'easeInOut', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
+        pauseOnHover: false, // Pause when user hovers the slide container
+        useDefaultCSS: false, // Add default CSS for positioning the slides
+        neverEnding: true // Create a 'neverending/repeating' slider effect.
+    };
+    //slider.nextSlide(); // Go to the next slide
+    //slider.prevSlide(); // Go to the previous slide
+    //slider.nextSlide(slidenumber); // Go to the given slide
+    $('a').on('click', function() {
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[class="' + scrollAnchor + '"]').offset().top;
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 1500);
+        return false;
+    });
+});
